6ad60819ef4162d55bdefcc40f82fecbb8060b83
Liam Hession <liam@gobootler.com>
2023-11-15 11:22:15 -0600

    Update style and content of business pages

--- build/build-admin.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Building admin app..."
-cd admin-app && npm install && npm run build && rm -rf ../eclipse-site/static/admin && mv build ../eclipse-site/static/admin
--- build/build-to-publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./build/build-admin.sh
-# After admin app is built, it gets added to the eclipse-site static directory.
-./build/build-hugo.sh
